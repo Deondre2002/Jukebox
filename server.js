8c667679e951ac92a6e5c8d5a7275300231393fc
Deondre2002 <dj232011@gmail.com>
2025-11-11 23:27:47 -0500

    Jukebox pro

--- server.js
+++ server.js
@@ -1,52 +1,13 @@
-import express from "express";
-import db from "./db/client.js";
+import app from "#app";
+import db from "#db/client";
+import dotenv from "dotenv";
 
-const app = express();
-const PORT = 3000;
+dotenv.config();
 
-app.use(express.json()) 
+const PORT = process.env.PORT ?? 3000;
 
-/
 await db.connect();
 
-
-
-app.get("/tracks", async (req, res) => {
-  try {
-    const result = await db.query("SELECT * FROM tracks;");
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Failed to fetch tracks" });
-  }
+app.listen(PORT, () => {
+  console.log(`🚀 Listening on port ${PORT}...`);
 });
-
-
-app.get("/tracks/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const result = await db.query("SELECT * FROM tracks WHERE id = $1;", [id]);
-    if (result.rows.length === 0) return res.status(404).json({ error: "Track not found" });
-    res.json(result.rows[0]);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Failed to fetch track" });
-  }
-});
-
-
-app.get("/playlists", async (req, res) => {
-  try {
-    const result = await db.query("SELECT * FROM playlist;");
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Failed to fetch playlists" });
-  }
-});
-
-
-app.get("/playlists/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const playlistRes = await db.query("SELECT * FROM playlist WHER*
