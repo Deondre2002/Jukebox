3d5f3d19e90dec67e0802652c771df1ca0cc1dc9
Deondre2002 <dj232011@gmail.com>
2025-11-11 23:29:05 -0500

    jukebox pro

--- app.js
+++ app.js
@@ -1,3 +1,71 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import db from "#db/client";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
+app.use(express.json());
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const result = await db.query(
+      "INSERT INTO users (username, password_hash) VALUES ($1, $2) RETURNING id, username;",
+      [username, hashedPassword]
+    );
+
+    const user = result.rows[0];
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(201).json({ user, token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Registration failed" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const result = await db.query("SELECT * FROM users WHERE username = $1;", [
+      username,
+    ]);
+
+    if (result.rows.length === 0) {
+      return res.status(401).json({ error: "Invalid username or password" });
+    }
+
+    const user = result.rows[0];
+
+    const validPassword = await bcrypt.compare(password, user.password_hash);
+
+    if (!validPassword) {
+      return res.status(401).json({ error: " Invalid username or password" });
+    }
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.json({
+      message: "Login successful!",
+      user: { id: user.id, username: user.username },
+      token,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Login failed" });
+  }
+});
+
 export default app;
